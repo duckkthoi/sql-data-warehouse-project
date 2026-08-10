3456090847dd47e09eaf7db7d8e3aaec89c2aa5f
duckkthoi <me1piece82@gmail.com>
2026-08-10 22:07:14 +0700

    Create init_database.sql for DataWarehouse setup
    Initial SQL script to create the DataWarehouse database and its schemas.

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+-- create database
+create database 'DataWarehouse';
+go
+
+use DataWarehousr;
+go
+
+--Create Schemas
+create schema bronze;
+go
+
+create schema silver;
+go
+
+create schema gold;
+go
